4f237fd149672d3cc98cb7f44af36b25a696838d
ndahai <gumojammbo@gmail.com>
2024-04-17 10:29:04 -0600

    Pantalla cuentas iniciada

--- Banco/HomeWindow.xaml.cs
+++ Banco/HomeWindow.xaml.cs
@@ -23,5 +23,30 @@ namespace Banco
         {
             InitializeComponent();
         }
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox listBox = (ListBox)sender;
+            if (listBox.SelectedItem != null)
+            {
+                ListBoxItem selectedItem = (ListBoxItem)listBox.SelectedItem;
+                StackPanel stackPanel = (StackPanel)selectedItem.Content;
+
+                foreach (var item in stackPanel.Children)
+                {
+                    if (item is TextBlock)
+                    {
+                        TextBlock textBlock = (TextBlock)item;
+                        if (textBlock.Text == "Cuentas")
+                        {
+                            AccountWindow acc = new AccountWindow();
+                            acc.Show();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+
     }
 }
