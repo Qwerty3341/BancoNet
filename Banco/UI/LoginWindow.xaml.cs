0f18d752262824acfc924d0efe2d86e9c9bd5865
Luciano Aarón <lucaaron00@gmail.com>
2024-04-23 22:52:08 -0600

    Implementando metodos para validar usuarios (falta logica de ventanas y base de datos)

--- Banco/UI/LoginWindow.xaml.cs
+++ Banco/UI/LoginWindow.xaml.cs
@@ -43,60 +43,71 @@ namespace Banco.UI
 
         }
 
+        //Metodo temporal en lo que se implementan las validaciones
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            /*
-             Aqui se validara que el usuario exista
-             */
-            string nombreUsuario = txtUsername.Text;
-            string contrasenia = txtPassword.Password;
 
-            if (ValidarUsuario(nombreUsuario, contrasenia))
-            {
                 HomeWindow homeWindow = new HomeWindow();
                 homeWindow.Show();
                 this.Close();
-            }
-            else
+        }
+
+            /*
+            private void Button_Click(object sender, RoutedEventArgs e)
             {
-                MessageBox.Show("Nombre o contraseña incorrectos");
+
+                 //Aqui se validara que el usuario exista
+
+                string nombreUsuario = txtUsername.Text;
+                string contrasenia = txtPassword.Password;
+
+                if (ValidarUsuario(nombreUsuario, contrasenia))
+                {
+                    HomeWindow homeWindow = new HomeWindow();
+                    homeWindow.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Nombre o contraseña incorrectos");
+                }
+
             }
-            
-        }
 
-        private bool ValidarUsuario(string username, string password)
-        {
-            return ValidarUsuarioCliente(username, password) || ValidarUsuarioEjecutivo(username, password) ;
-        }
-
-        private bool ValidarUsuarioCliente(string nombre, string password)
-        {
-            string connectionString = ConfigurationManager.ConnectionStrings["Banco.Properties.Settings.GestionBancoConnectionString"].ConnectionString;
-            using (SqlConnection conexion = new SqlConnection(connectionString))
+            private bool ValidarUsuario(string username, string password)
             {
-                SqlCommand cmd = new SqlCommand("SELECT COUNT(1) FROM cliente WHERE nombre=@nombre AND contrasenia=@password", conexion);
-                cmd.Parameters.AddWithValue("@nombre", nombre);
-                cmd.Parameters.AddWithValue("@password", password);
-
-                conexion.Open();
-                int count = Convert.ToInt32(cmd.ExecuteScalar());
-                return count == 1;
+                return ValidarUsuarioCliente(username, password) || ValidarUsuarioEjecutivo(username, password) ;
             }
-        }
 
-        private bool ValidarUsuarioEjecutivo(string rfc, string password)
-        {
-            string connectionString = ConfigurationManager.ConnectionStrings["Banco.Properties.Settings.GestionBancoConnectionString"].ConnectionString;
-            using (SqlConnection conexion = new SqlConnection(connectionString))
+            private bool ValidarUsuarioCliente(string nombre, string password)
             {
-                SqlCommand cmd = new SqlCommand("SELECT COUNT(1) FROM ejecutivo WHERE rfc=@rfc AND contrasenia=@password", conexion);
-                cmd.Parameters.AddWithValue("@rfc", rfc);
-                cmd.Parameters.AddWithValue("@password", password);
+                string connectionString = ConfigurationManager.ConnectionStrings["Banco.Properties.Settings.GestionBancoConnectionString"].ConnectionString;
+                using (SqlConnection conexion = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("SELECT COUNT(1) FROM cliente WHERE nombre=@nombre AND contrasenia=@password", conexion);
+                    cmd.Parameters.AddWithValue("@nombre", nombre);
+                    cmd.Parameters.AddWithValue("@password", password);
 
-                conexion.Open();
-                int count = Convert.ToInt32(cmd.ExecuteScalar());
-                return count == 1;
+                    conexion.Open();
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    return count == 1;
+                }
             }
+
+            private bool ValidarUsuarioEjecutivo(string rfc, string password)
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["Banco.Properties.Settings.GestionBancoConnectionString"].ConnectionString;
+                using (SqlConnection conexion = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("SELECT COUNT(1) FROM ejecutivo WHERE rfc=@rfc AND contrasenia=@password", conexion);
+                    cmd.Parameters.AddWithValue("@rfc", rfc);
+                    cmd.Parameters.AddWithValue("@password", password);
+
+                    conexion.Open();
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    return count == 1;
+                }
+            }
+            */
         }
-    }
 }
